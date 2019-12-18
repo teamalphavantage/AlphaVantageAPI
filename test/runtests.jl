8bb74b304d36ea49655859839933678dc3e851ea
ys939 <56899058+ys939@users.noreply.github.com>
2019-12-19 00:48:04 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -308,6 +308,77 @@ function download_weekly_appl_sts_with_logging_test()
 end
 
 
+function download_weekly_adjust_appl_sts_test()
+
+    # initialize -
+    my_current_dir = pwd()   # where am I?
+    path_to_config_file = my_current_dir*"/configuration/Configuration.json"
+
+    # build the api user model -
+    user_model_result = build_api_user_model(path_to_config_file)
+    if (typeof(user_model_result.value) == AVKError)
+        return false
+    end
+
+    # get the user model, we'll need this to make an API call -
+    user_model = user_model_result.value
+
+    # make an API call -
+    stock_symbol = "aapl"
+    data_type = :json
+    outputsize = :compact
+    api_call_result = execute_sts_adjust_weekly_api_call(user_model, stock_symbol; data_type = data_type, outputsize = outputsize, logger=nothing)
+
+    # check -
+    if (typeof(api_call_result.value) == DataFrame)
+        return true
+    end
+
+    # return -
+    return false
+end
+
+function download_weekly_adjust_appl_sts_with_logging_test()
+
+    # initialize -
+    my_current_dir = pwd()   # where am I?
+    path_to_config_file = my_current_dir*"/configuration/Configuration.json"
+
+    # create a simple logger -
+    path_to_log_file = my_current_dir*"/logs/log.txt"
+    io = open(path_to_log_file, "a+")
+    simple_logger = SimpleLogger(io, Logging.Debug)
+
+    # build the api user model -
+    user_model_result = build_api_user_model(path_to_config_file)
+    if (typeof(user_model_result.value) == AVKError)
+        @show typeof(user_model_result.value)
+        return false
+    end
+
+    # get the user model, we'll need this to make an API call -
+    user_model = user_model_result.value
+
+    # make an API call -
+    stock_symbol = "aapl"
+    data_type = :json
+    outputsize = :compact
+    api_call_result = execute_sts_adjust_weekly_api_call(user_model, stock_symbol; data_type = data_type, outputsize = outputsize, logger=simple_logger)
+
+    # close -
+    close(io)
+    @show typeof(api_call_result.value)
+
+    # check -
+    if (typeof(api_call_result.value) == DataFrame)
+        return true
+    else
+        @show(typeof(api_call_result.value))
+    end
+
+    # return -
+    return false
+end
 
 
 
@@ -336,4 +407,9 @@ end
 @testset "execute_sts_adjusted_api_call_set" begin
     @test download_daily_adjusted_appl_sts_test() == true
     #@test download_daily_adjusted_appl_sts_with_logging_test() == true
+end
+=#
+@testset "execute_sts_adjusted_api_call_set" begin
+    #@test download_monthly_appl_sts_test() == true
+    @test download_weekly_adjusted_appl_sts_with_logging_test() == true
 end
